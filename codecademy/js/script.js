f1d458edddae4b56a4f007f41a299b8c7e4fa146
bulesyk <wulining441@outlook.com>
2016-03-09 23:05:05 +0800

    修改:         codecademy/js/script.js

--- codecademy/js/script.js
+++ codecademy/js/script.js
@@ -1,1 +1,37 @@
+//初始化配置
+function config() {
+    $('.in-skill').style.backgroundColor = '#d5d5d5';
+    var cSkills = $('.c-skills');
+    for (var i=0,len=cSkills.length;i<len;i++) {
+        cSkills[i].style.display = 'none';
+    };
+    $('.noti').style.display = 'none';
+    $('.account').style.display = 'none';
+};
+function clickEvent() {
+    $('.in-skill').onclick = function(){
+        this.style.backgroundColor = '#d5d5d5';
+        $('.c-skill').style.backgroundColor = '#e8e8e8';
+        $('.in-skills').style.display = 'block';
+        var cSkills = $('.c-skills');
+        for (var i=0,len=cSkills.length;i<len;i++) {
+            cSkills[i].style.display = 'none';
+        };
+        return false;
+    };
+    $('.c-skill').onclick = function(){
+        this.style.backgroundColor = '#d5d5d5';
+        $('.in-skill').style.backgroundColor = '#e8e8e8';
+        $('.in-skills').style.display = 'none';
+        var cSkills = $('.c-skills');
+        for (var i=0,len=cSkills.length;i<len;i++) {
+            cSkills[i].style.display = 'block';
+        };
+        return false;
+    };
+};
 
+window.onload = function() {
+    config();
+    clickEvent();
+};
--- tool/tool.js
+++ tool/tool.js
@@ -1,26 +1,29 @@
 /* @移动动画
- * @para elemID(str) 需要移动的元素ID
+ * @para elem(elemObj) 需要移动的元素
  * @para finalX(number) 移动的最终X轴位置
  * @para finalY(number) 移动的最终Y轴位置
- * @oara startX(number) (可选) 默认为left:0 移动起点left的值
- * @para startY(number) (可选) 默认为top:0 移动起点top1的值
+ * @para constant(boolean) (可选) 默认为false 是否匀速移动
+ * @para speed(number) (可选) 默认为10 匀速移动的速度
+ * @oara startX(number) (可选) 默认为0 移动起点left的值
+ * @para startY(number) (可选) 默认为0 移动起点top的值
  * @para fps(number) (可选) 默认为30 不好解释,应该是动画移动的速度
  * @para interval(number) (可选) 默认为10 每次动画改变的间隔
  * @return true 动画完成后
  */
-function animation(elemID, finalX, finalY, startX, startY, fps, interval) {
-    var elem = document.getElementById(elemID);
+function animation(elem, finalX, finalY, constant, speed, startX, startY, fps, interval) {
     //设置默认的fps和interval
+    clearTimeout(elem.rotation);
     fps = fps || 30;
     interval = interval || 10;
     startX = startX || 0;
     startY = startY || 0;
+    constant = constant || false;
+    speed = speed || 5;
     //初始化位置
     elem.style.position = 'absolute';
     elem.parentNode.position = 'relative';
     elem.style.left = elem.style.left ? elem.style.left : startX;
     elem.style.top = elem.style.top ? elem.style.top : startY;
-    clearTimeout(elem.rotation);
     (function move() {
         var left = parseInt(elem.style.left),
             top = parseInt(elem.style.top);
@@ -29,17 +32,33 @@ function animation(elemID, finalX, finalY, startX, startY, fps, interval) {
             return true;
         };
         if (left < finalX) {
-            left += Math.ceil((finalX - left) / fps);
+            if (constant) {
+                left += speed;
+            } else {
+                left += Math.ceil((finalX - left) / fps);
+            };
         };
 
         if (left > finalX) {
-            left -= Math.ceil((left - finalX) / fps);
+            if (constant) {
+                left -= speed;
+            } else {
+                left -= Math.ceil((left - finalX) / fps);
+            };
         };
         if (top < finalY) {
-            top += Math.ceil((finalY - top) / fps);
+            if (constant) {
+                top += speed;
+            } else {
+                top += Math.ceil((finalY - top) / fps);
+            };
         };
         if (top > finalY) {
-            top -= Math.ceil((top - finalY) / fps);
+            if (constant) {
+                top -= speed;
+            } else {
+                top -= Math.ceil((top - finalY) / fps);
+            };
         };
         elem.style.left = left + 'px';
         elem.style.top = top + 'px';
@@ -123,14 +142,15 @@ function removeClass(elem, className) {
 /* @mini$ 取消了之前练习中utils的写法,之前不让用querySelectorAll(),可以选择到第5代,用空格区分
  * @只有二代选择时会返回选择第一代中的第一个元素的后代选择列表,三代选择会返回第二代的第一个元素的后代选择列表
  * para selecter(str) 部分css选择器
- * return elemObjList(用ID选择也是,只不过只有一个元素)
+ * return elemObjNodeList 如果只nodelist中只有一个elemObj,则返回elemObj
  */
 function $(selecter) {
     var reg = /^(.+)\s(.+)$/g.exec(selecter), //代数选择判断
         idReg = /^(#[\w_\-]+)$/, //ID选择
         tagReg = /^([a-z]+)$/, //标签选择
         classReg = /^(\.[\w_\-]+)$/, //类名选择
-        attrReg = /^([\w+]?)\[([\w_\-]+)(=['|"](.+)['|"])?\]$/; //属性值
+        attrReg = /^([\w+]?)\[([\w_\-]+)(=['|"](.+)['|"])?\]$/, //属性值
+        rootNode;
     if (!reg) {
         var para = {
             id: idReg.exec(selecter) ? idReg.exec(selecter)[1] : null,
@@ -144,8 +164,57 @@ function $(selecter) {
                 break;
             };
         };
-        return document.querySelectorAll(para);
+        return document.querySelectorAll(para).length === 1 ? document.querySelectorAll(para)[0] : document.querySelectorAll(para);
     } else {
-        return $(reg[1])[0].querySelectorAll(reg[2]);
+        if ($(reg[1]).length) {
+            rootNode = $(reg[1])[0];
+        } else {
+            rootNode = $(reg[1]);
+        }
+        return rootNode.querySelectorAll(reg[2]);
+    };
+};
+/* @事件代理
+ * @para elem(elemObj) 要添加的事件的元素的父元素
+ * @para tag(str) 要添加元素的标签名
+ * @para event(str) 要添加的事件名
+ * @para listener 监听函数
+ * @return 无
+ */
+function delegateEvent(elem, tag, event, listener) {
+    function parafn(e) {
+        var e = e || window.event,
+            target = e.target || e.srcElement;
+        if (target.tagName.toLowerCase() === tag.toLowerCase()) {
+            listener(target);
+        };
+    };
+    addEvent(elem, event, parafn);
+};
+//
+/* @添加事件
+ * @para elem(elemObj) 要添加的事件的元素
+ * @para event(str) 要添加的事件名称
+ * @para listener(fn) 触发事件后的监听函数
+ * @return 无
+ */
+function addEvent(elem, event, listener) {
+    if (elem.addEventListener) {
+        elem.addEventListener(event, listener, false);
+    } else {
+        elem.attachEvent('on' + event, listener);
+    };
+};
+/* @取消事件
+ * @para elem(elemObj) 要取消的事件的元素
+ * @para event(str) 要取消的事件名称
+ * @para listener(fn) 要取消事件的监听函数
+ * @return 无
+ */
+function removeEvent(elem, event, listener) {
+    if (elem.removeEventListener) {
+        elem.removeEventListener(event, listener);
+    } else {
+        elem.detachEvent('on' + event, listener);
     };
 };
