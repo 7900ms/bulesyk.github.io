03ea8f0bfc45cc1fd7617e9815659a273c98b707
bulesyk <wulining441@outlook.com>
2016-03-24 17:58:54 +0800

    commit

--- 2016task/task-2-22/script.js
+++ 2016task/task-2-22/script.js
@@ -3,6 +3,9 @@ var root = document.getElementsByTagName('div')[0],
     postBtn = document.getElementById('post'),
     searchText = document.getElementById('search-text'),
     search = document.getElementById('search'),
+    deleteBtn = document.getElementById('delete'),
+    insertText = document.getElementById('insert-text'),
+    insertBtn = document.getElementById('insert'),
     list = [];
 function preOrder(elem) {
     if (elem) {
@@ -31,10 +34,14 @@ function autoChangeColor(list) {
         autoChangeColor(list);
     }, 500);
 }
+// 绑定事件
 preBtn.addEventListener('click', function() {
     clearTimeout(setTimeoutId);
     if (list[i - 1]) {
         list[i - 1].style.removeProperty('background-color');
+    }
+    if (document.querySelector('.clicked')) {
+        document.querySelector('.clicked').classList.remove('clicked');
     }
     list = [];
     preOrder(root);
@@ -46,10 +53,14 @@ postBtn.addEventListener('click', function() {
     if (list[i - 1]) {
         list[i - 1].style.removeProperty('background-color');
     }
+    if (document.querySelector('.clicked')) {
+        document.querySelector('.clicked').classList.remove('clicked');
+    }
     list.reverse();
     i = 0, len = list.length;
     autoChangeColor(list);
 })
+// 查找
 search.addEventListener('click', function(e) {
     clearTimeout(setTimeoutId);
     if (list[i - 1]) {
@@ -60,15 +71,15 @@ search.addEventListener('click', function(e) {
     var text = searchText.value;
     i = 0, len = list.length;
     (function autoChangeColor() {
-        if (list[i - 1]) {
-            list[i - 1].style.removeProperty('background-color');
+        if (document.querySelector('.clicked')) {
+            document.querySelector('.clicked').classList.remove('clicked');
         }
         if (i === len) {
             alert('未找到选择元素!')
             return;
         }
-        list[i].style.backgroundColor = '#0f0';
-        if (list[i].firstChild.nodeValue.toLowerCase().replace(/^\s+|\s+$/g,'') == text.toLowerCase()) {
+        list[i].classList.add('clicked');
+        if (list[i].firstChild.nodeValue.toLowerCase().replace(/^\s+|\s+$/g, '') == text.toLowerCase()) {
             list[i].style.backgroundColor = '#00f';
             return;
         }
@@ -77,4 +88,47 @@ search.addEventListener('click', function(e) {
             autoChangeColor(list);
         }, 500);
     })();
-})
+});
+// 删除元素
+deleteBtn.addEventListener('click', function(e) {
+    var clickedElem = document.querySelector('.clicked');
+    clickedElem.parentNode.removeChild(clickedElem);
+});
+// 插入元素
+insertBtn.addEventListener('click', function(e) {
+    clearTimeout(setTimeoutId);
+    if (list[i - 1]) {
+        list[i - 1].style.removeProperty('background-color');
+    }
+    var value = insertText.value;
+    var clickedElem = document.querySelector('.clicked');
+    var newElem = document.createElement('div');
+    if (!clickedElem) {
+        alert("请选择元素!");
+        return;
+    }
+    if (!value) {
+        alert("请输入值!");
+        return;
+    }
+    newElem.style.width = '50%';
+    newElem.style.height = Math.floor(clickedElem.offsetHeight * 0.48) + 'px';
+    newElem.innerHTML = value;
+    clickedElem.appendChild(newElem);
+});
+// 绑定click事件
+(function divClick() {
+    if (list.length === 0) {
+        preOrder(root);
+    }
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].addEventListener('click', function(e) {
+            var e = e || window.event,
+                target = e.target || e.srcElement;
+            if (document.querySelector('.clicked')) {
+                document.querySelector('.clicked').classList.remove('clicked');
+            }
+            target.classList.add('clicked');
+        });
+    }
+})();
