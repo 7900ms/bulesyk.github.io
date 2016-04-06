464e5c1024b2b4a974f82704bcea137dc07fb460
bulesyk <wulining441@outlook.com>
2016-04-07 00:13:31 +0800

    commit

--- tool/wTool.js
+++ tool/wTool.js
@@ -1,5 +1,5 @@
 var w = (function() {
-	// prototype
+	// WTool
 	WTool.prototype = {
 		constructor: WTool,
 		// 继承自Array
@@ -220,9 +220,9 @@ var w = (function() {
 			}
 		},
 		// 事件委托
-		delegateEvent: function(parent, targetSelector, event, listener, config) {
-			if (!parent || !targetSelector || !event || !listener) return;
-			var len = parent.length;
+		delegateEvent: function(targetSelector, event, listener, config) {
+			if (!targetSelector || !event || !listener) return;
+			var len = this.length;
 			this['deleListener' + listener.name] = function(e) {
 				var tool = new WTool();
 				var e = tool.getEvent(e);
@@ -242,6 +242,7 @@ var w = (function() {
 			if (!event || !listener) return;
 			this.removeEvent(event, this['deleListener' + listener.name]);
 		},
+		// 添加样式
 		addStyle: function(config, elem) {
 			if (!config) return;
 			var that = elem || this;
@@ -259,6 +260,7 @@ var w = (function() {
 			}
 			return that;
 		},
+		// 删除样式
 		removeStyle: function(config, elem) {
 			if (!config) return;
 			var that = elem || this;
@@ -277,7 +279,9 @@ var w = (function() {
 			}
 			return that;
 		},
-		animation: function(elem, finalX, finalY, interval) {
+		// 移动动画
+		animation: function(finalX, finalY, interval,elem) {
+			var elem  = elem || this[0];
 			if (elem.setTimeoutId) {
 				clearTimeout(elem.setTimeoutId);
 			}
@@ -286,7 +290,6 @@ var w = (function() {
 				elem.style.top = '0';
 				elem.style.left = '0';
 			}
-			interval = interval || 10;
 			(function autoMove() {
 				var left = parseInt(elem.style.left),
 					top = parseInt(elem.style.top);
@@ -312,7 +315,9 @@ var w = (function() {
 				elem.setTimeoutId = setTimeout(autoMove, interval);
 			})();
 		},
-		resetPos: function(elem, left, top) {
+		// 重置位置
+		resetPos: function(left, top,elem) {
+			elem = elem || this[0];
 			elem.style.position = 'absolute';
 			elem.style.left = left + 'px';
 			elem.style.top = top + 'px';
@@ -320,10 +325,12 @@ var w = (function() {
 		carousel: function(containerConfig, imgsConfig, ruleConfig) {
 			var carousel = new Carousel(containerConfig, imgsConfig, ruleConfig);
 			return carousel;
+		},
+		formsGenerate:function (containerConfig,formsConfig,ruleConfig) {
+			return new FormsGenerate(containerConfig,formsConfig,ruleConfig);
 		}
 
 	}
-
 	function WTool(config, count) {
 		for (var key in config) {
 			var elem = document.createElement(key);
@@ -338,13 +345,51 @@ var w = (function() {
 				this.push(elem);
 			}
 		}
+		function checkInput(para,value) {
+			if (typeof para === 'string') {
+				this[0][para] = value;
+			}
+			if (Object.prototype.toString.call(value) === '[object Array]') {
+				var len = value.length;
+				for (var i = 0; i < len; i++) {
+					if (this[i]) {
+						this[i][para]=value[i];
+					}
+				}
+			}
+		}
+		Object.defineProperty(this,'id',{
+			get:function () {
+				return this[0].id;
+			},
+			set:function (id) {
+				checkInput.call(this,'id',id);
+			}
+		});
+		Object.defineProperty(this,'innerHTML',{
+			get:function () {
+				return this[0].innerHTML;
+			},
+			set:function (innerHTML) {
+				checkInput.call(this,'innerHTML',innerHTML);
+			}
+		});
+		Object.defineProperty(this,'className',{
+			get:function () {
+				return this[0].className;
+			},
+			set:function (className) {
+				checkInput.call(this,'className',className);
+			}
+		});
 	}
+	// 轮播图
 	Carousel.prototype = new WTool();
-
 	function Carousel(containerConfig, imgs, cssConfig, conf) {
 		var that = this,
 			id = containerConfig.id,
 			tool = new WTool();
+		// 创建元素
 		var containerBox = this.createElement('div', containerConfig);
 		var imgsWapper = this.createElement('div', {
 			id: id + '-imgs-wapper'
@@ -415,6 +460,7 @@ var w = (function() {
 			height: cssConfig.height,
 			position: 'absolute'
 		}, imgsWapper);
+		// 添加初始化元素
 		containerBox.appendChild(imgsWapper);
 		containerBox.appendChild(spansWapper);
 		containerBox.appendChild(leftArrow);
@@ -447,9 +493,9 @@ var w = (function() {
 					w('#' + id + '-imgs-wapper').w('.foil').removeStyle(['z-index']);
 					w('#' + id + '-imgs-wapper').w('.foil').removeClass('foil');
 				}
-				that.resetPos(imgsWapper, 0, 0);
-				that.resetPos(now[0], 0, 0);
-				that.resetPos(w('#' + id + '-imgs-wapper .number' + nextNumber)[0], direct * width, 0);
+				w(imgsWapper).resetPos(0, 0);
+				now.resetPos(0, 0);
+				w('#' + id + '-imgs-wapper .number' + nextNumber).resetPos( direct * width, 0);
 				now.addClass('foil');
 				now.removeClass('now');
 				active.removeClass('active');
@@ -458,7 +504,7 @@ var w = (function() {
 				w('#' + id).w('.now').addStyle({
 					zIndex: '1'
 				});
-				that.animation(imgsWapper, -direct * width, 0, 10);
+				w(imgsWapper).animation(-direct * width, 0, 10);
 			}
 			// 自动移动图片
 			function autoMove(direct, interval) {
@@ -523,6 +569,155 @@ var w = (function() {
 		this.containerBox = containerBox;
 		this.start = start;
 		this.stop = stop;
+	}
+	// 自动表单生成
+	FormsGenerate.prototype = {
+		__proto__:new WTool(),
+		changeColor: function(parentElem, text, color) {
+			parentElem.querySelector('.hint').innerHTML = text;
+			parentElem.querySelector('.hint').style.color = color;
+			parentElem.querySelector('input').style.borderColor = color;
+		}
+	}
+	function FormsGenerate(containerConfig,formsConfig,ruleConfig) {
+		var id = containerConfig.id;
+		var that = this;
+		var form = this.createElement('form',containerConfig);
+		w(form).addStyle(ruleConfig);
+		var sheet = document.styleSheets[0];
+		sheet.insertRule('#' +id+ `{
+			text-align:right;
+			width:400px;
+		}`,0);
+		sheet.insertRule('#' +id+' .input-wapper' +`{
+			height:4em;
+		}`,0);
+		sheet.insertRule('#' +id+' label' +`{
+			font-weight:bold;
+		}`,0);
+		sheet.insertRule('#' +id+' .forms-hide'+`{
+			display:none !important;
+		}`,0);
+		sheet.insertRule('#' +id+' input'+`{
+			margin-left: 10px;
+			width: 300px;
+			border-radius:5px;
+			padding:5px 10px;
+			border 1px soild #818181;
+		}`,0);
+		sheet.insertRule('#' +id+'  .hint'+`{
+			display:block;
+			text-align:left;
+			padding-left:100px;
+		}`,0);
+		sheet.insertRule('#' +id+'  .forms-btn'+`{
+			border-radius:5px;
+			padding:5px 10px;
+		}`,0);
+		// 判断传入的input个数
+		for (var key in formsConfig) {
+				// 创建from和input
+				var inputWapper = this.createElement('div',{
+					id:formsConfig[key].id +'input-wapper',
+					class:'input-wapper'
+				});
+				for (var innerkey in formsConfig[key]) {
+					if (innerkey==='id' || innerkey==='type' || innerkey==='label') {
+						continue;
+					}
+					inputWapper[innerkey] = formsConfig[key][innerkey];
+				}
+				var input = this.createElement('input',{
+					id:formsConfig[key].id,
+					type:formsConfig[key].type
+				});
+				var label = this.createElement('label',{
+					for:formsConfig[key].id
+				});
+				label.innerHTML = formsConfig[key].label;
+				var hint = this.createElement('span',{
+					class:'hint forms-hide'
+				});
+				w(hint).addStyle({
+					paddingLeft:parseInt(form.style.width)-300 +'px'
+				});
+				hint.innerHTML = formsConfig[key].rules;
+				// 默认的判断函数
+				input.validator = function(validator) {
+					var value = this.value.replace(/^\s+|\s+$/g, ''),
+						type = this.id;
+					if (!validator) {
+						// 默认的验证程序
+						validator = function() {
+							if (!value) {
+								return 'empty';
+							}
+							switch (type) {
+								case 'name':
+									var chineseReg = /[^\x00-\xff]/g;
+									value = value.replace(/^\s+|\s+$/g, '').replace(chineseReg, 'zz');
+									var reg = /^.{4,16}$/;
+									return reg.test(value);
+								case 'email':
+									var reg = /^[\w_-]+@[\w_-]+\.com$/;
+									return reg.test(value);
+								case 'phone':
+									var reg = /^\w{11}$/;
+									return reg.test(value);
+								default:
+									return true;
+							}
+						}
+					}
+					return validator.call(this);
+				}
+				inputWapper.appendChild(label);
+				inputWapper.appendChild(input);
+				inputWapper.appendChild(hint);
+				form.appendChild(inputWapper);
+		}
+		var btn = this.createElement('button',{
+			type:'submit',
+			class:'forms-btn'
+		});
+		btn.innerHTML = '提交';
+		form.appendChild(btn);
+		function start() {
+			// focus时的事件
+			function focusEvent(e) {
+				w(this.parentNode).w('.hint').removeClass('forms-hide');
+				that.changeColor(this.parentNode,this.parentNode.rules, '#818181');
+				this.style.removeProperty('border-color');
+			}
+			w('#'+id +' input').addEvent('focus',focusEvent);
+			// blur时的事件
+			function blurEvent(e) {
+				var validatorResult = this.validator(this.parentNode.validator);
+				// 如果允许为空
+				if (this.parentNode.empty) {
+					if (validatorResult === 'empty') {
+						w(this.parentNode).w('.hint').addClass('forms-hide');
+						that.changeColor(this.parentNode, '选填', '#818181');
+						this.style.removeProperty('border-color');
+						return;
+					}
+				} else {
+					if (validatorResult === 'empty') {
+						that.changeColor(this.parentNode, '不能为空', this.parentNode.faileColor);
+						return;
+					}
+				}
+				if (validatorResult) {
+					that.changeColor(this.parentNode, this.parentNode.success, this.parentNode.successColor);
+					this.parentNode.inputValue = this.value;
+				} else {
+					that.changeColor(this.parentNode, this.parentNode.faile, this.parentNode.faileColor);
+				}
+			}
+			w('#'+id +' input').addEvent('blur',blurEvent);
+		}
+		this.formsContainerBox = form;
+		this.start = start;
 	}
 	// 返回一个指向返回WTool对象的函数
 	return function(selector) {
